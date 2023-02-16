c365d5c2b5d731978731fbd3abba349e8fa47ac7
Remigiusz Mikolajewicz <remigiusz.mikolajewicz@espeo.eu>
2023-02-16 09:46:25 +0100

    navbar layout

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from 'react-native';
+
+const isAndroid = Platform.OS === 'android';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.navbar}>
+          <Text>Navbar</Text>
+        </View>
+        <View style={styles.main}>
+          <Text>Main</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  navbar: {
+    backgroundColor: 'red',
+    padding: 16,
+  },
+  main: {
+    flex: 1,
+    backgroundColor: 'blue',
+    padding: 16,
   },
 });
